ea1e2bff561f739362b0436f8e4fff7efd03dbb1
Samuel JS Wright <swrig109@uncc.edu>
2023-02-19 13:51:02 -0500

    updated broken link (that I broke earlier)

--- 1-create-cluster/1-2-install-tools.sh
+++ 1-create-cluster/1-2-install-tools.sh
@@ -12,7 +12,8 @@ eksctl version
 # kubectl
 echo ""
 echo "Installing kubectl ..."
-curl -o kubectl https://amazon-eks.s3.us-east-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --client
